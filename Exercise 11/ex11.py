8456c1dfac1889b23e3d6a0e427c80aefac0c5c5
Mayur Mallya <mayurmallya7@gmail.com>
2018-07-18 16:35:19 +0530

    Add files via upload

---
+++ Exercise 11/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you in inches?", end = ' ')
+height = input()
+print("How much do you weigh in Kgs?", end = ' ')
+weight = input()
+
+print(f"So, Syou're {age} years old, {height} inches tall and {weight} Kgs heavy.") 
