3972d32f30b2e1a4fd5374b2c9944b1728b30505
Mayur Mallya <mayurmallya7@gmail.com>
2018-08-18 16:00:21 +0530

    Add files via upload

---
+++ Exercise 40/ex40-0.py
@@ -0,0 +1,8 @@
+import myself
+
+myself.apple()
+print(mystuff.tangerine) 
+
+# mystuff['apple'] == get apple from dict
+# mystuff.apple() == get apple from the module
+# mystuff.tangerine == same thing, it's just a variable
