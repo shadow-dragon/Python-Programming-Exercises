1283ef5010daad87d96f55ade5057079245fe138
Mayur Mallya <mayurmallya7@gmail.com>
2018-08-28 15:52:58 +0530

    Delete ex11.py

--- Exercise 11/ex11.py
+++
@@ -1,8 +0,0 @@
-print("How old are you?", end = ' ')
-age = input()
-print("How tall are you in inches?", end = ' ')
-height = input()
-print("How much do you weigh in Kgs?", end = ' ')
-weight = input()
-
-print(f"So, Syou're {age} years old, {height} inches tall and {weight} Kgs heavy.") 
