56e50edef0c1899a8dcb90d1460a642aa407b7f4
Mayur Mallya <mayurmallya7@gmail.com>
2018-08-29 17:30:53 +0530

    Delete ex12.py

--- Exercise 12/ex12.py
+++
@@ -1,5 +0,0 @@
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh in Kgs? ")
-
-print(f"So, you're {age} years old, {height} inches tall and {weight} Kgs heavy.") 
